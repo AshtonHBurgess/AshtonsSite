1a45639b6e2fab3e5cb879dda78de74a419226f5
AshtonBurgess <w0465511@nscc.ca>
2023-03-29 11:04:41 -0300

    big update to attempt to live

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -56,7 +56,7 @@
 
                                     <div class="card text-center">
                                         <div class="card-header">
-                                            <h2>LIVE ! I&#39m Ashton, and this is a basic laravel site</h2>
+                                            <h2>LIVE !! I&#39m Ashton, and this is a basic laravel site</h2>
                                         </div>
 
 
